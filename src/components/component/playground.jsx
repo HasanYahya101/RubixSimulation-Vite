48cb786be23f1cd1bb7e755f4939c2ce98033b7c
Hasan Yahya <hasna786yahya@gmail.com>
2024-07-11 23:31:17 +0500

    refactor: Adjust margin-top in Rubik's Cube controls

--- src/components/component/playground.jsx
+++ src/components/component/playground.jsx
@@ -263,7 +263,7 @@ const RubiksCube = () => {
           display: flex;
           flex-direction: column;
           align-items: center;
-          margin-top: 40px;
+          margin-top: 50px;
         }
         .control-row {
           display: flex;
